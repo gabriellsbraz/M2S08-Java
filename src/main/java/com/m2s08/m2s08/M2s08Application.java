7d753c0bb0093645911f991d3bd0353ae425129a
gabriellsbraz <gabriel_lsb@hotmail.com>
2023-12-22 07:24:10 -0300

    [M02S08] Ex 01 - Mini-Projeto Checkin - Criação do projeto

---
+++ src/main/java/com/m2s08/m2s08/M2s08Application.java
@@ -0,0 +1,13 @@
+package com.m2s08.m2s08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2s08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2s08Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/m2s08/m2s08/M2s08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2s08.m2s08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2s08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
